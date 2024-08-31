94dbb112db7e04c02a80a7047eb31bb048f064a6
Prakash <gatiyalap@gmail.com>
2024-08-31 13:43:07 +0530

    Update index

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "http://35.154.171.190/:8000",
+  baseURL: "http://35.154.171.190:8000",
   responseType: "json",
 });
 
